02e21e7a0eb396ea9d76d7b26f3136be6d5f31e2
atinuke ilesanmi <atinukeilesanmi921@gmail.com>
2024-03-22 16:55:49 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Form from './Component/Form.jsx';
-import './App.css'
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+import Form from "./Component/Form";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='reactForm'>
-       <Form/>  
+      <div className="Form">
+        <Form />
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/form.jsx
+++ src/Component/form.jsx
@@ -1,13 +1,66 @@
-import "./Form.css"
-
-
+import { useState } from "react";
+import "./form.module.css";
 
 const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>React Form</h1>
-      <div className="card"></div>
-    </div>
+    <form action="">
+      <div>
+        <h1>React Form</h1>
+        <div>
+        <label htmlFor="">First Name:</label>
+            <br />
+          <input
+            name="First name"
+            type="First name"
+            placeholder="Enter your first Name"
+          />
+        </div>
+        <br />
+        <div>
+        <label htmlFor="">Last Name:</label>
+            <br />
+          <input
+            name="Last name"
+            type="Last name"
+            placeholder="Enter your last Name"
+          />
+        </div>
+        <br />
+        <div>
+        <label htmlFor="">Username:</label>
+            <br />
+          <input
+            name="UserName"
+            type="UserName"
+            placeholder="Enter your username"
+          />
+        </div>
+        <br />
+        <label htmlFor="">Gender:</label>
+            <br />
+        <select name="gender" id="Gender">
+         <option value="Male">Male</option>
+         <option value="female">Female</option>
+        </select>
+
+        <br />
+        <br />
+        <div>
+            <label htmlFor="">Password:</label>
+            <br />
+          <input
+            input
+            name="password"
+            type="password"
+            placeholder="*********"
+          />
+        </div>
+        <br />
+      </div>
+    </form>
   );
 };
 
